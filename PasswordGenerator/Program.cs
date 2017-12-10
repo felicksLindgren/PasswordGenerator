f0f5b58f38f1b450b1d3beab5bccfa3485e84f7d
Felix Lindgren <felix.lindgren@preciofishbone.se>
2017-12-10 19:33:27 +0100

    StartUp

---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(System.Web.Security.Membership.GeneratePassword(20, 10));
+            Console.ReadLine();
+        }
+    }
+}
